7b59da6a09c74ddae3c9b114c1e1365dba66b73b
JhoanBurbano <jhoanburbano@unicauca.edu.co>
2022-01-11 00:21:32 -0500

    rutas funcionales - backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,10 @@
-const express =require ('express');
-const  morgan =require ('morgan')
+const express = require('express');
+const morgan = require('morgan');
+const routes = require('./src/routes');
 const server = express();
-server.name ="API"
+server.name = 'API';
 
-require('./db')
+require('./db');
 server.use(express.urlencoded({ extended: false, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 // server.use(cookieParser());
@@ -11,23 +12,24 @@ server.use(express.json({ limit: '50mb' }));
 server.use(morgan('dev'));
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:300*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
+	res.header('Access-Control-Allow-Origin', 'http://localhost:300*');
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
 });
 
-require('./src/routes')
-
 //MANEJO DE ERRORES
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-    const status = err.status || 500;
-    const message = err.message || err;
-    console.error(err);
-    res.status(status).send(message);
-  });
+server.use((err, req, res, next) => {
+	// eslint-disable-line no-unused-vars
+	const status = err.status || 500;
+	const message = err.message || err;
+	console.error(err);
+	res.status(status).send(message);
+});
 
 //
-  server.set('port', process.env.PORT || 3001)
-  server.listen(server.get('port'), ()=>console.log(`I'm in http://localhost:${server.get('port')}`))
+server.use('/', routes);
+
+server.set('port', process.env.PORT || 3001);
+server.listen(server.get('port'), () => console.log(`I'm in http://localhost:${server.get('port')}`));
