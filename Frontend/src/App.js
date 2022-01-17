41bff86e65f31a0ff90ac74e87b0eb71e3778c1a
Agustin Sartorio <sartorio85@gmail.com>
2022-01-17 19:59:00 -0300

    navBar hecha

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import GlobalStyle from "./IndexStyled.jsx";
+import Home from './Components/Home/Home'
 
 function App() {
   return (
     <div>
-      <GlobalStyle/>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- Frontend/src/IndexStyled.jsx
+++ Frontend/src/IndexStyled.jsx
@@ -1,8 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
   body {
-  background:black;
   margin: 0%;
   width: 100vh;
   }
