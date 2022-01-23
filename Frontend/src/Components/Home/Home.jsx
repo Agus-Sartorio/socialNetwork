719916a45292e272b84ea98d9978209c46f3471c
Naomi <naomiirisd@gmail.com>
2022-01-23 17:19:34 -0300

    usuario loggeado

--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -1,30 +1,13 @@
-import { useUserAuth } from "../Context/UserContext";
+
 import NavBar from "../NavBar/NavBar";
 import SideBar from "../SideBar/SideBar";
 
 
 export default function Home() {
-    const { user, logOut} = useUserAuth()
-    console.log(user)
-
-    const handleLogOut = async() => {
-        try{
-           await logOut();
-
-        }catch(error){
-            console.log(error.message)
-        }
-    }
     return (
         <>
             <NavBar />
             <SideBar />
-<<<<<<< HEAD
-            <button onClick={handleLogOut}>logout</button>
-          
-        </div>
-=======
         </>
->>>>>>> Develop
     )
 }
