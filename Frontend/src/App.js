f275a7eaaf14316569965b0e71e26455f9fcdfd8
Agustin Sartorio <sartorio85@gmail.com>
2022-01-25 14:30:00 -0300

    empenzando cambios en el logIn

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import GlobalStyle from "./IndexStyled.jsx";
 import LogIn from './Components/FormLogIn/LogIn';
-import FormSignUp from './Components/FormSignUp/FormSignUp';
 import Home from './Components/Home/Home';
 import LandingPage from './Components/LandingPage/LandingPage';
 import Edit from './Components/Edit/Index';
@@ -20,7 +19,6 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/edit" element={<Edit />} />
-          <Route path="/signup" element={<FormSignUp />} />
           <Route path="/home" element={<Home />} />
           <Route path="/suggestions" element={<Suggestions />} />
           <Route path='/profile/:id' element={<Profile />} />
