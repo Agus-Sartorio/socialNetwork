3048a8f526df7cf139c5c7c938de9f21039f7bf6
Agustin Sartorio <sartorio85@gmail.com>
2022-01-31 11:55:56 -0300

    bug arreglado del crear post y pageNotFound

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -9,6 +9,7 @@ import { UserAuthContextProvider } from "./Components/Context/UserContext";
 import { ProtectedRoute } from './Components/ProtectedRoute'
 import Profile from './Components/Profile/Profile.jsx';
 import { ViewProfile } from "./Components/MyProfile/ViewProfile.jsx";
+import NotFound from "./Components/NotFound/NotFound.jsx";
 function App() {
   return (
     <div className='app'>
@@ -18,13 +19,13 @@ function App() {
       <UserAuthContextProvider>
         <Routes>
           <Route path="/" element={<LogIn />} />
-          <Route path="/edit" element={<ProtectedRoute><Edit/></ProtectedRoute>} />
-          <Route path="/chat" element={<ProtectedRoute><Chat/></ProtectedRoute>} />
-          <Route path="/home" element={<ProtectedRoute><Home/></ProtectedRoute>} />
-          <Route path="/suggestions" element={<ProtectedRoute><Suggestions/></ProtectedRoute>} />
+          <Route path="/edit" element={<ProtectedRoute><Edit /></ProtectedRoute>} />
+          <Route path="/chat" element={<ProtectedRoute><Chat /></ProtectedRoute>} />
+          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/suggestions" element={<ProtectedRoute><Suggestions /></ProtectedRoute>} />
           <Route path='/profile/:id' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-          <Route path='/myprofile' element={<ProtectedRoute><ViewProfile/></ProtectedRoute>} />
-          <Route path='*' element={<div>NOTfound</div>}/>
+          <Route path='/myprofile' element={<ProtectedRoute><ViewProfile /></ProtectedRoute>} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </UserAuthContextProvider>
     </div>
