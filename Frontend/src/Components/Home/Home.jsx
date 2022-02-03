61b140459c6eec3952af65e786a79e0020edffeb
unknown <dufainder@gmail.com>
2022-02-02 20:54:06 -0500

    Guardando para actualizar

--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState, useRef} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AllPost, getCleanHome, getMyId} from "../../actions";
 import CrearPost from "../CrearPost/CrearPost";
 import Layout from '../Layout/Layout'
 import PostContainer from "../PostContainer/PostContainer";
-import io from 'socket.io-client';
+import io,{Socket} from 'socket.io-client';
 
 
 // socket.on('conectado',function(data){
@@ -13,33 +13,60 @@ import io from 'socket.io-client';
 
 //////////////////////
 export default function Home() {
+    const renderizadoControl = useRef(0);
+    const [Socket, setSocket] = useState('ok');
    
-// const socket = io(`http://localhost:3002`);
+  
 //  socket.on('connection',()=>{
 //      console.log('estoy conectado desde el back')
 //  })
-
-
+const user = useSelector((state => state.myId));
+  if (user.length !== 0)console.log(user, 'Mi Id' )
     /////////////////////////// abajo no
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(AllPost());
         dispatch(getMyId())
+        // dispatch(getMyProfile())
+
         return () => {
             dispatch(getCleanHome())
         }
     }, [dispatch]);
 
     const posts = useSelector((state => state.allPost))
+    
+////////////////////////////arriba no se nota
+    useEffect(()=>{
+        let socket =io('http://localhost:3002')
+        setSocket(socket);
+        console.log(Socket,'el gran Socket')
+        socket.on('connection',()=>{
+        console.log('estoy conectado desde el back')   
+        
+        
+    }) 
+      },[ ])
 
-////////////////////////////
-    // useEffect(()=>{
-    //     const socket = io(`http://localhost:3002`);
-    //     socket.on('connection',()=>{
-    //     console.log('estoy conectado desde el back')
-    //  })
-    //   },[])
+      useEffect(() => {
+          if(renderizadoControl.current===0){
+              renderizadoControl.current= renderizadoControl.current +1;
+              return
+          };
+     
+         
+            Socket.emit("addUser", user.id);
+            Socket.on("getUsers", (users) => {
+               
+                console.log(users,"usuarios conectados")
+            
+                //   setOnlineUsers(
+            //     user.followings.filter((f) => users.some((u) => u.userId === f))
+            //   );
+            });
+         
+      }, [user.id]);
 
     // if(socket !== null) { socket.on('conectado',function(data){
     //     console.log(data,'data socket')
