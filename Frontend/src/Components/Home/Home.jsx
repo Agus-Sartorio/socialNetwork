ce0845611a3b1942ff60e185bfe634e7ca0fc555
Edwinleon95 <ridergaleon@yahoo.com>
2022-02-04 21:49:03 -0500

    scrollinfinito

--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -1,28 +1,50 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { AllPost, getCleanHome, getMyId } from "../../actions";
+import { AllPost, getCleanHome } from "../../actions";
+import useNearScreen from "../../Hooks/useNearScreen";
 import CrearPost from "../CrearPost/CrearPost";
 import Layout from "../Layout/Layout";
 import PostContainer from "../PostContainer/PostContainer";
-
+import debounce from 'just-debounce-it';
 export default function Home() {
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(AllPost());
-    dispatch(getMyId());
+    const posts = useSelector((state => state.allPost))
+    const dispatch = useDispatch()
 
-    return () => {
-      dispatch(getCleanHome());
-    };
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(AllPost());
+        return () => {
+            dispatch(getCleanHome())
+          }
+    }, [dispatch]);
+    const [postsPerPage, setPostsPerPage] = useState(10);
+    const [currentPage] = useState(1);
 
-  const posts = useSelector((state) => state.allPost);
+    let lastPostInpage = currentPage * postsPerPage;
+    let firstPostInpage = lastPostInpage - postsPerPage
+    let currentPosts = posts?.slice(firstPostInpage, lastPostInpage)
+    const externalRef = useRef()
+    const {isNearScreen} = useNearScreen({
+        externalRef: !posts.length ? null : externalRef,
+        once: false
+      })
+    const debounceHandleNextPage = useCallback(debounce(
+        () => setPostsPerPage(postsPerPage + 10), 500
+      ), [postsPerPage])
+  
+      useEffect(function () {
+        if (isNearScreen) debounceHandleNextPage()
+      }, [debounceHandleNextPage, isNearScreen])
+    
 
-  return (
-    <Layout>
-      <CrearPost />
-      <PostContainer posts={posts} />
-    </Layout>
-  );
+
+    return (
+        <Layout>
+            <CrearPost />
+            {posts.length?
+            <PostContainer posts={currentPosts} />:<div>cargando</div>}
+            {postsPerPage >= posts.length? null:
+            <div id="visor" ref={externalRef}>te observo</div>}
+        </Layout>
+    )
 }
